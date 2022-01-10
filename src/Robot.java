c854ca95195506d46dc76ffed674bb750f22cc91
Kateryna-art <bentleyss@148@gmail.com>
2022-01-10 23:09:46 +0100

    ExamFile/Git

---
+++ src/Robot.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Robot {
+        public String robotName;
+        private double health=100;
+        String alphabet = "QWEASDZXC";
+        String damageKeys ="";
+
+        public Robot(String robotName) {
+            for (int i = 0; i < 5; i++) {
+                damageKeys = damageKeys + getRandomKey();
+            }
+        }
+
+        public double getHealth(){ return health;}
+
+        public void setHealth(double v) {this.health=health;}
+
+        public String getRobotName() {return robotName;}
+
+        char getRandomKey() {
+            int randIdx = new Random().nextInt(alphabet.length());
+            char randChar = alphabet.charAt(randIdx);
+            alphabet = alphabet.replace(String.valueOf(randChar), "");
+            return randChar;
+
+        }
+
+}
+
